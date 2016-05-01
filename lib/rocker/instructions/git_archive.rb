55cbbac20d1620e96256388abafa335f2efab853
Todd Lunter <tlunter@gmail.com>
2016-04-30 21:11:49 -0400

    Add GitArchive instruction

---
+++ lib/rocker/instructions/git_archive.rb
@@ -0,0 +1,40 @@
+module Rocker
+  module Instructions
+    # GitArchive instruction wraps the Add instruction
+    class GitArchive < Add
+      include Rocker::Util::LogHelper
+
+      attr_reader :repo_path, :container_path
+
+      def initialize(repo_path, container_path, options: {})
+        @repo_path = repo_path
+        @container_path = container_path
+        @options = options
+      end
+
+      def context
+        return @output if @output
+        @output = StringIO.new
+
+        IO.popen("git archive --format=tar #{branch}") do |io|
+          @output.write(io.read)
+        end
+
+        @output
+      end
+
+      def branch
+        options[:branch] || "master"
+      end
+
+      def hash_of_repo_path
+        context.rewind
+        sha256 = Digest::SHA256.new
+        sha256 << context.string
+        sha256.hexdigest
+      end
+    end
+  end
+end
+
+Rocker::DSL.register(:git_archive, Rocker::Instructions::GitArchive)
